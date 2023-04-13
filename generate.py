4e3f630c10a14312cc34788ad1f2f838ab8488a5
alsdudrla10 <alsdudrla10@kaist.ac.kr>
2023-04-13 23:33:50 +0900

    upload

--- generate.py
+++ generate.py
@@ -24,7 +24,7 @@ import classifier_lib
 # Proposed EDM-G++ sampler.
 
 def edm_sampler(
-    time_min, time_max, vpsde, dg_weight_1st_order, dg_weight_2nd_order, discriminator,
+    boosting, time_min, time_max, vpsde, dg_weight_1st_order, dg_weight_2nd_order, discriminator,
     net, latents, class_labels=None, randn_like=torch.randn_like,
     num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
@@ -38,23 +38,46 @@ def edm_sampler(
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
     t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
+    ## Settings for boosting
+    S_churn_manual = 4.
+    S_noise_manual = 1.000
+    period = 5
+    period_weight = 2
+    log_ratio = torch.tensor([np.inf] * latents.shape[0], device=latents.device)
+    S_churn_vec = torch.tensor([S_churn] * latents.shape[0], device=latents.device)
+    S_churn_max = torch.tensor([np.sqrt(2) - 1] * latents.shape[0], device=latents.device)
+    S_noise_vec = torch.tensor([S_noise] * latents.shape[0], device=latents.device)
+
     # Main sampling loop.
     x_next = latents.to(torch.float64) * t_steps[0]
     for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
         x_cur = x_next
+        S_churn_vec_ = S_churn_vec.clone()
+        S_noise_vec_ = S_noise_vec.clone()
+
+        if i % period == 0:
+            if boosting:
+                S_churn_vec_[log_ratio < 0.] = S_churn_manual
+                S_noise_vec_[log_ratio < 0.] = S_noise_manual
+
         # Increase noise temporarily.
-        gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
-        t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
+        # gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
+        gamma_vec = torch.minimum(S_churn_vec_ / num_steps, S_churn_max) if S_min <= t_cur <= S_max else torch.zeros_like(S_churn_vec_)
+        t_hat = net.round_sigma(t_cur + gamma_vec * t_cur)
+        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt()[:, None, None, None] * S_noise_vec_[:, None, None,None] * randn_like(x_cur)
+        #x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
 
         # Euler step.
         denoised = net(x_hat, t_hat, class_labels).to(torch.float64)
-        d_cur = (x_hat - denoised) / t_hat
+        d_cur = (x_hat - denoised) / t_hat[:, None, None, None]
         ## DG correction
         if dg_weight_1st_order != 0.:
-            discriminator_guidance = classifier_lib.get_grad_log_ratio(discriminator, vpsde, x_hat, t_hat, net.img_resolution, time_min, time_max, class_labels, log=False)
-            d_cur += dg_weight_1st_order * (discriminator_guidance / t_hat)
-        x_next = x_hat + (t_next - t_hat) * d_cur
+            discriminator_guidance, log_ratio = classifier_lib.get_grad_log_ratio(discriminator, vpsde, x_hat, t_hat, net.img_resolution, time_min, time_max, class_labels, log=True)
+            if boosting:
+                if i % period_weight == 0:
+                    discriminator_guidance[log_ratio < 0.] *= 2.
+            d_cur += dg_weight_1st_order * (discriminator_guidance / t_hat[:, None, None, None])
+        x_next = x_hat + (t_next - t_hat)[:, None, None, None] * d_cur
 
         # Apply 2nd order correction.
         if i < num_steps - 1:
@@ -64,7 +87,7 @@ def edm_sampler(
             if dg_weight_2nd_order != 0.:
                 discriminator_guidance = classifier_lib.get_grad_log_ratio(discriminator, vpsde, x_next, t_next, net.img_resolution, time_min, time_max, class_labels, log=False)
                 d_prime += dg_weight_2nd_order * (discriminator_guidance / t_next)
-            x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
+            x_next = x_hat + (t_next - t_hat)[:, None, None, None] * (0.5 * d_cur + 0.5 * d_prime)
 
     return x_next
 
@@ -103,6 +126,7 @@ def edm_sampler(
 @click.option('--dg_weight_2nd_order',     help='Weight of DG for 2nd prediction',       metavar='FLOAT',           type=click.FloatRange(min=0), default=0., show_default=True)
 @click.option('--time_min',                help='Minimum time[0,1] to apply DG', metavar='FLOAT',                   type=click.FloatRange(min=0., max=1.), default=0.01, show_default=True)
 @click.option('--time_max',                help='Maximum time[0,1] to apply DG', metavar='FLOAT',                   type=click.FloatRange(min=0., max=1.), default=1.0, show_default=True)
+@click.option('--boosting',                help='If true, dg scale up low log ratio samples', metavar='INT',        type=click.IntRange(min=0), default=0, show_default=True)
 
 ## Discriminator checkpoint
 @click.option('--pretrained_classifier_ckpt',help='Path of ADM classifier(latent extractor)',  metavar='STR',       type=str, default='/checkpoints/ADM_classifier/32x32_classifier.pt', show_default=True)
@@ -111,7 +135,7 @@ def edm_sampler(
 ## Discriminator architecture
 @click.option('--cond',                    help='Is it conditional discriminator?', metavar='INT',                  type=click.IntRange(min=0, max=1), default=0, show_default=True)
 
-def main(time_min, time_max, dg_weight_1st_order, dg_weight_2nd_order, cond, pretrained_classifier_ckpt, discriminator_ckpt, save_type, batch_size, do_seed, seed, num_samples, network_pkl, outdir, class_idx, device, **sampler_kwargs):
+def main(boosting, time_min, time_max, dg_weight_1st_order, dg_weight_2nd_order, cond, pretrained_classifier_ckpt, discriminator_ckpt, save_type, batch_size, do_seed, seed, num_samples, network_pkl, outdir, class_idx, device, **sampler_kwargs):
     ## Set seed
     if do_seed:
         import random
@@ -149,7 +173,7 @@ def main(time_min, time_max, dg_weight_1st_order, dg_weight_2nd_order, cond, pre
 
         ## Generate images.
         sampler_kwargs = {key: value for key, value in sampler_kwargs.items() if value is not None}
-        images = edm_sampler(time_min, time_max, vpsde, dg_weight_1st_order, dg_weight_2nd_order, discriminator, net, latents, class_labels, randn_like=torch.randn_like, **sampler_kwargs)
+        images = edm_sampler(boosting, time_min, time_max, vpsde, dg_weight_1st_order, dg_weight_2nd_order, discriminator, net, latents, class_labels, randn_like=torch.randn_like, **sampler_kwargs)
 
         ## Save images.
         images_np = (images * 127.5 + 128).clip(0, 255).to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy()
--- train.py
+++ train.py
@@ -17,6 +17,18 @@ def npz_concat(filenames):
             data = samples
     return data
 
+def npz_concat_cond(filenames):
+    for file in filenames:
+        samples = np.load(file)['samples']
+        label = np.load(file)['label']
+        try:
+            data = np.concatenate((data, samples))
+            data_label = np.concatenate((data_label, label))
+        except:
+            data = samples
+            data_label = label
+    return data, data_label
+
 class BasicDataset(data.Dataset):
   def __init__(self, x_np, y_np, transform=transforms.ToTensor()):
     super(BasicDataset, self).__init__()
@@ -31,11 +43,27 @@ class BasicDataset(data.Dataset):
   def __len__(self):
     return len(self.x)
 
+class BasicDatasetCond(data.Dataset):
+  def __init__(self, x_np, y_np, cond_np, transform=transforms.ToTensor()):
+    super(BasicDatasetCond, self).__init__()
+
+    self.x = x_np
+    self.y = y_np
+    self.cond = cond_np
+    self.transform = transform
+
+  def __getitem__(self, index):
+    return self.transform(self.x[index]), self.y[index], self.cond[index]
+
+  def __len__(self):
+    return len(self.x)
+
 @click.command()
 @click.option('--savedir',                     help='Save directory',          metavar='PATH',    type=str, required=True,     default="/checkpoints/discriminator/cifar_uncond")
 @click.option('--gendir',                      help='Fake sample directory',   metavar='PATH',    type=str, required=True,     default="/samples/cifar_uncond_vanilla")
 @click.option('--datadir',                     help='Real sample directory',   metavar='PATH',    type=str, required=True,     default="/data/true_data.npz")
 @click.option('--img_resolution',              help='Image resolution',        metavar='INT',     type=click.IntRange(min=1),  default=32)
+@click.option('--cond',                        help='Is it conditional?',      metavar='INT',     type=click.IntRange(min=0),  default=0)
 
 @click.option('--pretrained_classifier_ckpt',  help='Path of classifier',      metavar='STR',     type=str,                    default='/checkpoints/ADM_classifier/32x32_classifier.pt')
 @click.option('--num_data',                    help='Num samples',             metavar='INT',     type=click.IntRange(min=1),  default=50000)
@@ -52,15 +80,30 @@ def main(**kwargs):
     os.makedirs(savedir, exist_ok=True)
 
     ## Prepare real data
-    real_data = np.load(datadir)['arr_0']
+    if not opts.cond:
+        real_data = np.load(datadir)['arr_0']
+    else:
+        real_data  = np.load(datadir)['samples']
+        real_label = np.load(datadir)['label']
+        real_label = np.eye(10)[real_label]
 
     ## Prepare fake data
-    if not os.path.exists(os.path.join(gendir, 'gen_data_for_discriminator_training.npz')):
-        filenames = np.sort(glob.glob(os.path.join(gendir, 'sample*.npz')))
-        gen_data = npz_concat(filenames)
-        np.savez_compressed(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'), samples=gen_data)
+    if not opts.cond:
+        if not os.path.exists(os.path.join(gendir, 'gen_data_for_discriminator_training.npz')):
+            filenames = np.sort(glob.glob(os.path.join(gendir, 'sample*.npz')))
+            gen_data = npz_concat(filenames)
+            np.savez_compressed(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'), samples=gen_data)
+        else:
+            gen_data = np.load(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'))['samples']
     else:
-        gen_data = np.load(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'))['samples']
+        if not os.path.exists(os.path.join(gendir, 'gen_data_for_discriminator_training.npz')):
+            filenames = np.sort(glob.glob(os.path.join(gendir, 'sample*.npz')))
+            gen_data, gen_label = npz_concat_cond(filenames)
+            np.savez_compressed(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'), samples=gen_data, label=gen_label)
+        else:
+            gen_data = np.load(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'))['samples']
+            gen_label = np.load(os.path.join(gendir, 'gen_data_for_discriminator_training.npz'))['label']
+            gen_label = gen_label[:opts.num_data]
 
     ## Combine the fake / real
     real_data = real_data[:opts.num_data]
@@ -69,12 +112,17 @@ def main(**kwargs):
     train_label = torch.zeros(train_data.shape[0])
     train_label[:real_data.shape[0]] = 1.
     transform = transforms.Compose([transforms.ToTensor()])
-    train_dataset = BasicDataset(train_data, train_label, transform)
+    if not opts.cond:
+        train_dataset = BasicDataset(train_data, train_label, transform)
+    else:
+
+        condition_label = np.concatenate((real_label, gen_label))
+        train_dataset = BasicDatasetCond(train_data, train_label, condition_label, transform)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=opts.batch_size, num_workers=0, shuffle=True, drop_last=True)
 
     ## Extractor & Disciminator
     pretrained_classifier = classifier_lib.load_classifier(opts.pretrained_classifier_ckpt, opts.img_resolution, opts.device, eval=False)
-    discriminator = classifier_lib.load_discriminator(None, opts.device, False, eval=False)
+    discriminator = classifier_lib.load_discriminator(None, opts.device, opts.cond, eval=False)
 
     ## Prepare training
     vpsde = classifier_lib.vpsde()
@@ -89,7 +137,11 @@ def main(**kwargs):
         num_data = 0
         for data in train_loader:
             optimizer.zero_grad()
-            inputs, labels = data
+            if not opts.cond:
+                inputs, labels = data
+            else:
+                inputs, labels, cond = data
+                cond = cond.to(opts.device)
             inputs = inputs.to(opts.device)
             labels = labels.to(opts.device)
             inputs = scaler(inputs)
@@ -103,8 +155,10 @@ def main(**kwargs):
             ## Forward
             with torch.no_grad():
                 pretrained_feature = pretrained_classifier(perturbed_inputs, timesteps=t, feature=True)
-            label_prediction = discriminator(pretrained_feature, t, sigmoid=True).view(-1)
-
+            if not opts.cond:
+                label_prediction = discriminator(pretrained_feature, t, sigmoid=True).view(-1)
+            else:
+                label_prediction = discriminator(pretrained_feature, t, sigmoid=True, condition=cond).view(-1)
             ## Backward
             out = loss(label_prediction, labels)
             out.backward()
